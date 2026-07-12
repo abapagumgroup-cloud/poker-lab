d632553a12f6970daa12138daf7fc2f1c6325415
Aba Pagum <settingbots@gmail.com>
2026-07-12 17:21:27 +0300

    הוסף מאמן GTO — החלטות +EV, מגמת רווחיות, אימון אוטומטי (SW v2)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='pokerlab-v1';
+const CACHE='pokerlab-v2';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./favicon-32.png','./icon-180.png','./icon-192.png','./icon-512.png','./icon-maskable-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim()));});
